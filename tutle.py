c26771a77cbe9420e4de95710a3831859b5f2eb3
bozostupiddied <125477109+bozostupiddied@users.noreply.github.com>
2023-02-15 08:47:28 -0700

    Create tutle.py

---
+++ tutle.py
@@ -0,0 +1,49 @@
+import turtle
+
+t = turtle.Turtle()
+s = turtle.Screen()
+r=50
+number = 0
+while number <1:
+    number +=1
+    t.speed(100)
+    t.circle(50)
+    t.forward(200)
+    t.right(180)
+    t.forward(130)
+    t.right(90)
+    t.forward(-90)
+    t.right(50)
+    t.forward(142)
+    t.right(40.5)
+    t.forward(20)
+    t.right(89)
+    t.forward(100)
+    t.right(89)
+    t.forward(183)
+    t.right(90)
+    t.forward(160)
+    t.circle(20)
+    t.right(180)
+    t.forward(50)
+    t.right(67)
+    t.forward(98)
+    t.right(149)
+    t.forward(70)
+    t.left(85)
+    t.forward(70)
+    t.right(130)
+    t.forward(50)
+    t.right(-60)
+number=10
+while number !=0:
+
+
+    number = number - 1
+
+
+
+
+
+s.exitonclick()
+
